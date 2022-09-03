55ab65b039a56a43fdc8d362472c0426dfc507c5
rrrrat <112762929+rrrrat@users.noreply.github.com>
2022-09-04 01:12:02 +0800

    Update settings.py

--- project_manage/settings.py
+++ project_manage/settings.py
@@ -110,8 +110,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '843636329@qq.com'
-EMAIL_HOST_PASSWORD = 'lobsykeiffshbahh'
+EMAIL_HOST_USER = 'email_u'
+EMAIL_HOST_PASSWORD = 'email_p'
 DEFAULT_FROM_EMAIL = '843636329@qq.com'
 
 CACHES = {
